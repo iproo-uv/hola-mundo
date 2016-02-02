e097c9bb6902051176c37e5d1d0b9551b9c66027
flopez <flopez@alertlogic.com>
2016-02-02 14:28:05 -0500

    Intial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Federico López Gómez
+// Version     : 0.0.0
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+    return 0;
+}
