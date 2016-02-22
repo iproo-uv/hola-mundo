b96f8850fbccc7c190081fab660d1a0796458314
JuanTux <cybera0840@gmail.com>
2016-02-22 16:30:39 -0500

    linux al poder

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,6 @@
 using namespace std;
 
 int main() {
-    cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+    cout << "!!!Hola mundo --->  Juan David" << endl; // prints !!!Hello World!!!
     return 0;
 }
